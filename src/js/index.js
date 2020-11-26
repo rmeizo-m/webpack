61d3081fc5ea1d35077a13d61d21fe47ee3ca81e
rmeizo-m <vinnikove21@gmail.com>
2020-11-26 18:50:01 +0300

    all ptoject

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../scss/main.scss'
+
+const btnBurger = document.querySelector('.navbar__btn')
+const menu = document.querySelector('.navbar__menu')
+
+btnBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.classList.toggle("open") 
+})
